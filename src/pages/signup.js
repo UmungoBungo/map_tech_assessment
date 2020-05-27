c759374c2c11417d6381186e22c54e6a7b56c080
UmungoBungo <m.mcgillivray@outlook.com>
2020-05-27 07:04:14 +0000

    removed min height from sign in form pages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,7 +5,7 @@ import SignUpForm from '../components/SignUp';
 
 const SignUpPage = () => (
   <Fragment>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div class="max-w-md w-full">
         <SignUpForm />
       </div>
