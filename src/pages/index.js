9b2f50f503e718f144a7af7e60352d7bcd8c62ad
UmungoBungo <m.mcgillivray@outlook.com>
2020-05-27 07:10:58 +0000

    minor styling on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,21 @@ import Img from "gatsby-image"
 import Layout from '../components/layout';
 
 const LandingPage = () => (
-  <Fragment>
-    <h1>Landing</h1>
-    <p>
-      The Landing Page is open to everyone, even though the user isn't
-      signed in.
-    </p>
-  </Fragment>
+    <Fragment>
+        <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+            <h1 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">Landing</h1>
+        </div>
+        <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+            <p className="ml-2 block text-sm leading-5 text-gray-300">
+                The Landing Page is open to everyone, even though the user isn't
+                signed in.
+            </p>
+        </div>
+    </Fragment>
 );
 
 export default () => (
-  <Layout>
-    <LandingPage />
-  </Layout>
+    <Layout>
+        <LandingPage />
+    </Layout>
 );
