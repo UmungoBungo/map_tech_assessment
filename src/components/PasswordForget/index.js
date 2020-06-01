77aa8ded5ad68f60840f74446b7b1ed122e4b912
UmungoBungo <m.mcgillivray@outlook.com>
2020-06-01 12:06:38 +0000

    Changed padlock color

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -5,93 +5,93 @@ import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const INITIAL_STATE = {
-  email: '',
-  error: null,
+    email: '',
+    error: null,
 };
 
 class PasswordForgetForm extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { ...INITIAL_STATE };
-  }
+        this.state = { ...INITIAL_STATE };
+    }
 
-  onSubmit = event => {
-    const { email } = this.state;
+    onSubmit = event => {
+        const { email } = this.state;
 
-    this.props.firebase
-      .doPasswordReset(email)
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
+        this.props.firebase
+            .doPasswordReset(email)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
 
-    event.preventDefault();
-  };
+        event.preventDefault();
+    };
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-  render() {
-    const { email, error } = this.state;
+    render() {
+        const { email, error } = this.state;
 
-    const isInvalid = email === '';
+        const isInvalid = email === '';
 
-    return (
-      <>
-        <div>
-          <FullTitleLogo />
-          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
-            Reset account password
+        return (
+            <>
+                <div>
+                    <FullTitleLogo />
+                    <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
+                        Reset account password
             </h2>
-        </div>
-        <form className="mt-8" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm">
-            <div>
-              <input
-                aria-label="Email address"
-                name="email"
-                type="email"
-                required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Email address"
-                onChange={this.onChange} />
-            </div>
-          </div>
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
-              disabled={isInvalid}
-              type="submit">
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg>
-              </span>
-              Reset My Password
-              </button>
-          </div>
-          {error && <p>{error.message}</p>}
-        </form>
-      </>
-    );
-  }
+                </div>
+                <form className="mt-8" action="#" method="POST">
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm">
+                        <div>
+                            <input
+                                aria-label="Email address"
+                                name="email"
+                                type="email"
+                                required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Email address"
+                                onChange={this.onChange} />
+                        </div>
+                    </div>
+                    <div className="mt-6">
+                        <button
+                            type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
+                            disabled={isInvalid}
+                            type="submit">
+                            {isInvalid && <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <svg className="h-5 w-5 text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                                </svg>
+                            </span>}
+                            Reset My Password
+                        </button>
+                    </div>
+                    {error && <p>{error.message}</p>}
+                </form>
+            </>
+        );
+    }
 }
 
 const PasswordForgetLink = () => (
-  <div className="flex items-center">
-    <label className="ml-2 block text-sm leading-5">
-      <Link to={ROUTES.PASSWORD_FORGET}>
-        <span className="font-medium text-purple-400 hover:text-purple-200 focus:outline-none focus:underline transition ease-in-out duration-150">
-        Forgot your password?
+    <div className="flex items-center">
+        <label className="ml-2 block text-sm leading-5">
+            <Link to={ROUTES.PASSWORD_FORGET}>
+                <span className="font-medium text-purple-400 hover:text-purple-200 focus:outline-none focus:underline transition ease-in-out duration-150">
+                    Forgot your password?
         </span>
-      </Link>
-    </label>
-  </div>
+            </Link>
+        </label>
+    </div>
 );
 
 export default withFirebase(PasswordForgetForm);
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -91,7 +91,7 @@ class SignInFormBase extends Component {
                             disabled={isInvalid}
                             type="submit">
                             {isInvalid && <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                                <svg className="h-5 w-5 text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
                                     <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                 </svg>
                             </span>}
