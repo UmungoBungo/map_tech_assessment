97cb854c4a681c7db08671d1f9ff1fb994612d79
UmungoBungo <m.mcgillivray@outlook.com>
2020-06-01 09:07:41 +0000

    fixed sign in. Padlock icon linked to isInvalid

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -8,112 +8,104 @@ import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const INITIAL_STATE = {
-  email: '',
-  password: '',
-  error: null,
+    email: '',
+    password: '',
+    error: null,
 };
 
-const ERROR_CODE_ACCOUNT_EXISTS =
-  'auth/account-exists-with-different-credential';
-
-const ERROR_MSG_ACCOUNT_EXISTS = `
-  An account with an E-Mail address to
-  this social account already exists. Try to login from
-  this account instead and associate your social accounts on
-  your personal account page.
-`;
+// const ERROR_CODE_ACCOUNT_EXISTS =
+//     'auth/account-exists-with-different-credential';
 
 class SignInFormBase extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+    }
 
-    this.state = { ...INITIAL_STATE };
-  }
+    onSubmit = event => {
+        const { email, password } = this.state;
 
-  onSubmit = event => {
-    const { email, password } = this.state;
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                navigate(ROUTES.LANDING);
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
 
-    this.props.firebase
-      .doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        navigate(ROUTES.LANDING);
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
+        event.preventDefault();
+    };
 
-    event.preventDefault();
-  };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+    render() {
+        const { email, password, error } = this.state;
 
-  render() {
-    const { email, password, error } = this.state;
+        const isInvalid = password === '' || email === '';
 
-    const isInvalid = password === '' || email === '';
-
-    return (
-      <>
-        <div>
-          <FullTitleLogo />
-          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
-            Sign in to your account
+        return (
+            <>
+                <div>
+                    <FullTitleLogo />
+                    <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
+                        Sign in to your account
             </h2>
-        </div>
-        <form className="mt-8" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm">
-            <div>
-              <input
-                aria-label="Email address"
-                name="email"
-                type="email"
-                autoComplete="username"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Email address"
-                onChange={this.onChange} />
-            </div>
-            <div className="-mt-px">
-              <input
-                aria-label="Password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Password"
-                onChange={this.onChange} />
-            </div>
-          </div>
+                </div>
+                <form className="mt-8" onSubmit={this.onSubmit}>
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm">
+                        <div>
+                            <input
+                                aria-label="Email address"
+                                name="email"
+                                type="email"
+                                autoComplete="username"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Email address"
+                                onChange={this.onChange} />
+                        </div>
+                        <div className="-mt-px">
+                            <input
+                                aria-label="Password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Password"
+                                onChange={this.onChange} />
+                        </div>
+                    </div>
 
-          <div className="mt-6 flex items-center">
-            <SignUpLink />
-          </div>
+                    <div className="mt-6 flex items-center">
+                        <SignUpLink />
+                    </div>
 
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
-              disabled={isInvalid}
-              type="submit">
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg>
-              </span>
-                Sign in
-              </button>
-          </div>
-          <div className="mt-6 flex items-center">
-            <PasswordForgetLink />
-          </div>
-          {error && <p>{error.message}</p>}
-        </form>
-      </>
-    );
-  }
+                    <div className="mt-6">
+                        <button
+                            type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
+                            disabled={isInvalid}
+                            type="submit">
+                            {isInvalid && <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                                </svg>
+                            </span>}
+                            Sign in
+                        </button>
+                        {error && <p className="ml-2 block text-sm leading-5 text-gray-300">{error.message}</p>}
+                    </div>
+                    <div className="mt-6 flex items-center">
+                        <PasswordForgetLink />
+                    </div>
+                </form>
+            </>
+        );
+    }
 }
 
 const SignInForm = withFirebase(SignInFormBase);
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -7,176 +7,171 @@ import * as ROLES from '../../constants/roles';
 import FullTitleLogo from '../ImageRendering/full_title'
 
 const INITIAL_STATE = {
-  username: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
-  isAdmin: false,
-  error: null,
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    isAdmin: false,
+    error: null,
 };
 
 const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
 
 const ERROR_MSG_ACCOUNT_EXISTS = `
   An account with this E-Mail address already exists.
-  Try to login with this account instead. If you think the
-  account is already used from one of the social logins, try
-  to sign in with one of them. Afterward, associate your accounts
-  on your personal account page.
+  Try to login with this account instead.
 `;
 
 class SignUpFormBase extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { ...INITIAL_STATE };
-  }
-
-  onSubmit = event => {
-    const { username, email, passwordOne, isAdmin } = this.state;
-    const roles = {};
-
-    if (isAdmin) {
-      roles[ROLES.ADMIN] = ROLES.ADMIN;
+        this.state = { ...INITIAL_STATE };
     }
 
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
-      .then(authUser => {
-        // Create a user in your Firebase realtime database
-        return this.props.firebase.user(authUser.user.uid).set({
-          username,
-          email,
-          roles,
-        });
-      })
-      .then(() => {
-        return this.props.firebase.doSendEmailVerification();
-      })
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        navigate(ROUTES.LANDING);
-      })
-      .catch(error => {
-        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
-          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+    onSubmit = event => {
+        const { username, email, passwordOne, isAdmin } = this.state;
+        const roles = {};
+
+        if (isAdmin) {
+            roles[ROLES.ADMIN] = ROLES.ADMIN;
         }
 
-        this.setState({ error });
-      });
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                // Create a user in your Firebase realtime database
+                return this.props.firebase.user(authUser.user.uid).set({
+                    username,
+                    email,
+                    roles,
+                });
+            })
+            .then(() => {
+                return this.props.firebase.doSendEmailVerification();
+            })
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                navigate(ROUTES.LANDING);
+            })
+            .catch(error => {
+                if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+                    error.message = ERROR_MSG_ACCOUNT_EXISTS;
+                }
 
-    event.preventDefault();
-  };
+                this.setState({ error });
+            });
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+        event.preventDefault();
+    };
 
-  onChangeCheckbox = event => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-  render() {
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      isAdmin,
-      error,
-    } = this.state;
+    onChangeCheckbox = event => {
+        this.setState({ [event.target.name]: event.target.checked });
+    };
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === '' ||
-      email === '' ||
-      username === '';
+    render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
 
-    return (
-      <>
-        <div>
-          <FullTitleLogo />
-          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
-            Sign up for an account
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === '';
+
+        return (
+            <>
+                <div>
+                    <FullTitleLogo />
+                    <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-300">
+                        Sign up for an account
             </h2>
-        </div>
-        <form className="mt-8" onSubmit={this.onSubmit}>
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm">
-            <div>
-              <input
-                aria-label="username"
-                name="username"
-                type="text"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Username"
-                onChange={this.onChange} />
-            </div>
-            <div className="-mt-px">
-              <input
-                aria-label="Email"
-                name="email"
-                type="email"
-                autoComplete="username"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Email Address"
-                onChange={this.onChange} />
-            </div>
-            <div className="-mt-px">
-              <input
-                aria-label="Password"
-                name="passwordOne"
-                type="password"
-                autoComplete="new-password"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Password"
-                onChange={this.onChange} />
-            </div>
-            <div className="-mt-px">
-              <input
-                aria-label="Confirm Password"
-                name="passwordTwo"
-                type="password"
-                autoComplete="new-password"
-                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Confirm Password"
-                onChange={this.onChange} />
-            </div>
-          </div>
+                </div>
+                <form className="mt-8" onSubmit={this.onSubmit}>
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm">
+                        <div>
+                            <input
+                                aria-label="username"
+                                name="username"
+                                type="text"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Username"
+                                onChange={this.onChange} />
+                        </div>
+                        <div className="-mt-px">
+                            <input
+                                aria-label="Email"
+                                name="email"
+                                type="email"
+                                autoComplete="username"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Email Address"
+                                onChange={this.onChange} />
+                        </div>
+                        <div className="-mt-px">
+                            <input
+                                aria-label="Password"
+                                name="passwordOne"
+                                type="password"
+                                autoComplete="new-password"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Password"
+                                onChange={this.onChange} />
+                        </div>
+                        <div className="-mt-px">
+                            <input
+                                aria-label="Confirm Password"
+                                name="passwordTwo"
+                                type="password"
+                                autoComplete="new-password"
+                                required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                                placeholder="Confirm Password"
+                                onChange={this.onChange} />
+                        </div>
+                    </div>
 
-
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
-              disabled={isInvalid}
-              type="submit">
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg>
-              </span>
-                Sign up
-              </button>
-            {error && <p>{error.message}</p>}
-          </div>
-        </form>
-      </>
-    );
-  }
+                    <div className="mt-6">
+                        <button
+                            type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out"
+                            disabled={isInvalid}
+                            type="submit">
+                            {isInvalid && <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <svg className="h-5 w-5 text-purple-500 group-hover:text-purple-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                                </svg>
+                            </span>}
+                            Sign up
+                        </button>
+                        {error && <p className="ml-2 block text-sm leading-5 text-gray-300">{error.message}</p>}
+                    </div>
+                </form>
+            </>
+        );
+    }
 }
 
 const SignUpLink = () => (
-  <div className="flex items-center">
-    <label className="ml-2 block text-sm leading-5 text-gray-300">
-      {"Don't have an account? "}
-      <Link to={ROUTES.SIGN_UP}>
-        <span className="font-medium text-purple-400 hover:text-purple-200 focus:outline-none focus:underline transition ease-in-out duration-150">
-          Sign Up
+    <div className="flex items-center">
+        <label className="ml-2 block text-sm leading-5 text-gray-300">
+            {"Don't have an account? "}
+            <Link to={ROUTES.SIGN_UP}>
+                <span className="font-medium text-purple-400 hover:text-purple-200 focus:outline-none focus:underline transition ease-in-out duration-150">
+                    Sign Up
         </span>
-      </Link>
-    </label>
-  </div>
+            </Link>
+        </label>
+    </div>
 );
 
 export default withFirebase(SignUpFormBase);
