ca33ee32cac67402080c256fd03a1d80812a3c66
UmungoBungo <m.mcgillivray@outlook.com>
2020-06-07 06:04:29 +0000

    added low poly landing image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 import * as ROUTES from '../constants/routes';
 import Layout from '../components/layout';
+import LandingImage from '../components/ImageRendering/landing_image'
 
 const LandingPage = () => (
     <Fragment>
@@ -17,9 +18,9 @@ const LandingPage = () => (
                     <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div class="sm:text-center lg:text-left">
                             <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                                Data to enrich your{" "}
+                                Make My{" "}
                                 <br class="xl:hidden" />
-                                <span class="text-indigo-600">online business
+                                <span class="text-indigo-600">Games
                                 </span>
                             </h2>
                             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -42,7 +43,8 @@ const LandingPage = () => (
                 </div>
             </div>
             <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+                {/* <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" /> */}
+                    <LandingImage />
             </div>
         </div>
     </Fragment>
