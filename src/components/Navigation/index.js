2e377b7b5a5ee435dea482bccb97eb332542c8d8
UmungoBungo <m.mcgillivray@outlook.com>
2020-06-07 06:13:05 +0000

    added z-index to profile dropdown

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -108,7 +108,7 @@ const NavigationAuth = ({ authUser, toggleMobMenu, mobMenuOpen, closeMobMenu, to
                             From: "transform opacity-100 scale-100"
                             To: "transform opacity-0 scale-95"
                         --> */}
-                        {profileMenuOpen && <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                        {profileMenuOpen && <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg z-40">
                             <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" onBlur={closeProfileMenu}>
                                 <Link to={ROUTES.ACCOUNT} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Your Account</Link>
                                 <Link onClick="#" className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Settings</Link>
