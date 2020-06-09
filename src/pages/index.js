6b16e5d0140238796a015d812402d77cd77fdd9f
UmungoBungo <m.mcgillivray@outlook.com>
2020-06-09 04:25:47 +0000

    removed latin placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const LandingPage = () => (
                                 </span>
                             </h2>
                             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                                Here is some placeholder text that will be updated in the near future. Check back regularly for updates.
                             </p>
                             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div class="rounded-md shadow">
