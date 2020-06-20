255f63584668a1b7fefc370c53b1be8d663e901e
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-06-20 17:10:25 +0800

    creating idea page

--- src/pages/idea-page.js
+++ src/pages/idea-page.js
@@ -1,101 +1,55 @@
 import React, { Fragment } from 'react';
+import { Link } from 'gatsby';
+import * as ROUTES from '../constants/routes';
 import Layout from '../components/layout';
 
-const IdeaPage = () => (
+const IdeaPage = (title) => (
     <Fragment>
         <section className="text-gray-400 body-font">
             <div className="flex items-center justify-center py-12 px-0 sm:px-6 lg:px-8">
                 <div className="max-w-4xl w-full">
                     <div>
-                        <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Applicant Information
-                        </h3>
-                        <p class="max-w-2xl text-sm leading-5 text-gray-500">
-                            Personal details and application.
-                        </p>
+                        <h2 class="text-2xl text-center leading-6 font-medium text-gray-900">
+                            'props.title'
+                        </h2>
                     </div>
                     <div class="mt-5 border-t border-gray-200 pt-5">
                         <dl>
-                            <div class=" sm:grid sm:grid-cols-3 sm:gap-4">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    Full name
+                            <div class="sm:grid sm:grid-cols-3 sm:gap-4">
+                            <dt class="text-md leading-5 uppercase font-medium text-teal-400">
+                                    Amount pledged
                                 </dt>
-                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    Margot Foster
+                                <dd class="mt-1 text-md leading-5 font-medium text-teal-400 sm:mt-0 sm:col-span-2">
+                                    'props.pledged'
                                 </dd>
                             </div>
                             <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    Application for
+                                <dt class="text-sm leading-5 font-medium uppercase text-gray-500">
+                                    TLDR
                                 </dt>
                                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    Backend Developer
+                                'props.short'
                                 </dd>
                             </div>
                             <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    Email address
-                                </dt>
-                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    margotfoster@example.com
-                                </dd>
-                            </div>
-                            <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    Salary expectation
-                                </dt>
-                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    $120,000
-                                </dd>
-                            </div>
-                            <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    About
+                                <dt class="text-sm leading-5 font-medium uppercase text-gray-500">
+                                    Details
                                 </dt>
                                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                                     Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
                                 </dd>
                             </div>
-                            <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    Attachments
-                                </dt>
-                                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    <ul class="border border-gray-200 rounded-md">
-                                        <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm leading-5">
-                                            <div class="w-0 flex-1 flex items-center">
-                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                                </svg>
-                                                <span class="ml-2 flex-1 w-0 truncate">
-                                                    resume_back_end_developer.pdf
-                                        </span>
-                                            </div>
-                                            <div class="ml-4 flex-shrink-0">
-                                                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out">
-                                                    Download
-                                        </a>
-                                            </div>
-                                        </li>
-                                        <li class="border-t border-gray-200 pl-3 pr-4 py-3 flex items-center justify-between text-sm leading-5">
-                                            <div class="w-0 flex-1 flex items-center">
-                                                <svg class="flex-shrink-0 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                                </svg>
-                                                <span class="ml-2 flex-1 w-0 truncate">
-                                                    coverletter_back_end_developer.pdf
-                                                </span>
-                                            </div>
-                                            <div class="ml-4 flex-shrink-0">
-                                                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out">
-                                                    Download
-                                                </a>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </dd>
-                            </div>
+
+
+                            {/* <div class="mt-8 sm:grid sm:mt-5 sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                <div/>
+                                <div/>
+                                <Link to={ROUTES.SIGN_UP} className="ml-4 px-3 py-2 rounded-md text-sm font-medium uppercase leading-5 cta-button focus:shadow-outline-purple">Pledge</Link>
+                            </div> */}
                         </dl>
+                    </div>
+                    <div class="mt-8 flex justify-end">
+                        <Link to={ROUTES.SIGN_UP} className="px-3 py-2 mr-8 sm:mr-0 rounded-md text-sm font-medium uppercase leading-5 cta-button focus:shadow-outline-purple">Pledge</Link>
                     </div>
                 </div>
             </div>
