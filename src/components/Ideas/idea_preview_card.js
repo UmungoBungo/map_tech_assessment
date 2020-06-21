dd4f08b9be71247c4e15b6e361ada700afa5b2a4
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-06-21 14:58:58 +0800

    added more devinition to idea cards

--- src/components/Ideas/idea_preview_card.js
+++ src/components/Ideas/idea_preview_card.js
@@ -3,13 +3,13 @@ import { Link } from 'gatsby';
 import * as ROUTES from '../../constants/routes';
 
 const IdeaPreviewCard = ({ ideaTitle, ideaContent, pledged }) => (
-    <div className="overflow-hidden shadow rounded-lg my-12">
+    <div className="overflow-hidden shadow-xl rounded-lg my-12 border border-black">
         <Link to={ROUTES.IDEA_PAGE}>
             <h3 className="bg-white px-4 py-5 sm:px-6 text-lg leading-6 font-medium text-gray-900">
                 {ideaTitle}
             </h3>
         </Link>
-        <div className="bg-gray-200 px-4 py-5 sm:p-6 text-sm leading-5 text-gray-900">
+        <div className="bg-indigo-100 px-4 py-5 sm:p-6 text-sm leading-5 text-gray-900">
             <div>
                 {ideaContent}
             </div>
