63cefe48505dc53c9b740ba45d0f53104de88d4c
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-06-28 13:16:29 +0800

    graphql reading from firestore

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,31 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     { resolve: `gatsby-source-filesystem`, options: { path: `./src/data/` } },
+    {
+      resolve: 'gatsby-source-firestore',
+      options: {
+        credential: require("./firebase.json"),
+        types: [
+          {
+            type: 'Idea',
+            collection: 'ideas',
+            map: doc => ({
+              title: doc.title,
+              tldr: doc.tldr,
+              description: doc.description,
+              user___NODE: doc.userId
+            }),
+          },
+          {
+            type: 'User',
+            collection: 'users',
+            map: doc => ({
+              username: doc.username,
+              email: doc.email
+            }),
+          },
+        ],
+      },
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ const LandingPage = () => (
                 </div>
             </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={landingGIF} alt="make my games landing image" />
+                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={landingGIF} alt="make my games landing" />
             </div>
         </div>
     </Fragment>
