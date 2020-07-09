6963748a3c6439b0409667ded16e4624044690c4
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-07-09 14:04:31 +0800

    updated env variables for firestore

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,18 @@ module.exports = {
     {
       resolve: 'gatsby-source-firestore',
       options: {
-        credential: require("./firebase.json"),
+        credential: JSON.parse(JSON.stringify({
+          type: "service_account",
+          project_id: "make-my-games",
+          private_key_id: process.env.GATSBY_FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.GATSBY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          client_email: process.env.GATSBY_FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.GATSBY_FIREBASE_CLIENT_ID,
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url: process.env.GATSBY_FIREBASE_CLIENT_x509_CERT_URL
+        })),
         types: [
           {
             type: 'Idea',
