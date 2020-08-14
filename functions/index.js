58d92bb0a59c27f0eb3093be3171d2c36c7ed9a6
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-08-14 10:56:03 +0800

    stripe customer function was overwriting instead of updating

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
     const intent = await stripe.setupIntents.create({
         customer: customer.id,
     });
-    await admin.firestore().collection('users').doc(user.uid).set({
+    await admin.firestore().collection('users').doc(user.uid).update({
         stripe: {
             customer_id: customer.id,
             setup_secret: intent.client_secret
@@ -34,31 +34,31 @@ exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
  * When adding the payment method ID on the client,
  * this function is triggered to retrieve the payment method details.
  */
-exports.addPaymentMethodDetails = functions.firestore
-    .document('/users/{userId}/stripe/payment_methods/{pushId}')
-    .onCreate(async (snap, context) => {
-        try {
-            const paymentMethodId = snap.data().id;
-            const paymentMethod = await stripe.paymentMethods.retrieve(
-                paymentMethodId
-            );
-            await snap.ref.set(paymentMethod);
-            // Create a new SetupIntent so the customer can add a new method next time.
-            const intent = await stripe.setupIntents.create({
-                customer: paymentMethod.customer,
-            });
-            await snap.ref.parent.parent.set(
-                {
-                    setup_secret: intent.client_secret,
-                },
-                { merge: true }
-            );
-            return;
-        } catch (error) {
-            await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
-            await reportError(error, { user: context.params.userId });
-        }
-    });
+// exports.addPaymentMethodDetails = functions.firestore
+//     .document('/users/{userId}/stripe/payment_methods/{pushId}')
+//     .onCreate(async (snap, context) => {
+//         try {
+//             const paymentMethodId = snap.data().id;
+//             const paymentMethod = await stripe.paymentMethods.retrieve(
+//                 paymentMethodId
+//             );
+//             await snap.ref.set(paymentMethod);
+//             // Create a new SetupIntent so the customer can add a new method next time.
+//             const intent = await stripe.setupIntents.create({
+//                 customer: paymentMethod.customer,
+//             });
+//             await snap.ref.parent.parent.set(
+//                 {
+//                     setup_secret: intent.client_secret,
+//                 },
+//                 { merge: true }
+//             );
+//             return;
+//         } catch (error) {
+//             await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+//             await reportError(error, { user: context.params.userId });
+//         }
+//     });
 
 /**
  * When a payment document is written on the client,
@@ -69,38 +69,38 @@ exports.addPaymentMethodDetails = functions.firestore
 
 // [START chargecustomer]
 
-exports.createStripePayment = functions.firestore
-    .document('users/{userId}/stripe/payments/{pushId}')
-    .onCreate(async (snap, context) => {
-        const { amount, currency, payment_method } = snap.data();
-        try {
-            // Look up the Stripe customer id.
-            const customer = (await snap.ref.parent.parent.get()).data().customer_id;
-            // Create a charge using the pushId as the idempotency key
-            // to protect against double charges.
-            const idempotencyKey = context.params.pushId;
-            const payment = await stripe.paymentIntents.create(
-                {
-                    amount,
-                    currency,
-                    customer,
-                    payment_method,
-                    off_session: false,
-                    confirm: true,
-                    confirmation_method: 'manual',
-                },
-                { idempotencyKey }
-            );
-            // If the result is successful, write it back to the database.
-            await snap.ref.set(payment);
-        } catch (error) {
-            // We want to capture errors and render them in a user-friendly way, while
-            // still logging an exception with StackDriver
-            console.log(error);
-            await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
-            await reportError(error, { user: context.params.userId });
-        }
-    });
+// exports.createStripePayment = functions.firestore
+//     .document('users/{userId}/stripe/payments/{pushId}')
+//     .onCreate(async (snap, context) => {
+//         const { amount, currency, payment_method } = snap.data();
+//         try {
+//             // Look up the Stripe customer id.
+//             const customer = (await snap.ref.parent.parent.get()).data().customer_id;
+//             // Create a charge using the pushId as the idempotency key
+//             // to protect against double charges.
+//             const idempotencyKey = context.params.pushId;
+//             const payment = await stripe.paymentIntents.create(
+//                 {
+//                     amount,
+//                     currency,
+//                     customer,
+//                     payment_method,
+//                     off_session: false,
+//                     confirm: true,
+//                     confirmation_method: 'manual',
+//                 },
+//                 { idempotencyKey }
+//             );
+//             // If the result is successful, write it back to the database.
+//             await snap.ref.set(payment);
+//         } catch (error) {
+//             // We want to capture errors and render them in a user-friendly way, while
+//             // still logging an exception with StackDriver
+//             console.log(error);
+//             await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+//             await reportError(error, { user: context.params.userId });
+//         }
+//     });
 
 // [END chargecustomer]
 
@@ -110,16 +110,16 @@ exports.createStripePayment = functions.firestore
  *
  * @see https://stripe.com/docs/payments/accept-a-payment-synchronously#web-confirm-payment
  */
-exports.confirmStripePayment = functions.firestore
-    .document('users/{userId}/stripe/payments/{pushId}')
-    .onUpdate(async (change, context) => {
-        if (change.after.data().status === 'requires_confirmation') {
-            const payment = await stripe.paymentIntents.confirm(
-                change.after.data().id
-            );
-            change.after.ref.set(payment);
-        }
-    });
+// exports.confirmStripePayment = functions.firestore
+//     .document('users/{userId}/stripe/payments/{pushId}')
+//     .onUpdate(async (change, context) => {
+//         if (change.after.data().status === 'requires_confirmation') {
+//             const payment = await stripe.paymentIntents.confirm(
+//                 change.after.data().id
+//             );
+//             change.after.ref.set(payment);
+//         }
+//     });
 
 /**
  * When a user deletes their account, clean up after them
