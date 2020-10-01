cda5230fc91c9a3ecb919d1068d27ca6e80430e1
DESKTOP-UNARPAC\Matt <m.mcgillivray@outlook.com>
2020-10-01 11:25:14 +0800

    distances calculated and added to infowindow. Minor styling changes

--- src/components/GoogleMap/map.js
+++ src/components/GoogleMap/map.js
@@ -15,30 +15,33 @@ function Map(props) {
   const createMarkers = () => {
     const bounds = new window.google.maps.LatLngBounds();
     const markerArray = []
-    allLocation.edges.forEach( (edge) => {
+    allLocation.edges.forEach((edge) => {
       const coordinates = new window.google.maps.LatLng(edge.node.lat, edge.node.long)
-      const infowindow = new window.google.maps.InfoWindow({
-        content: `${edge.node.name} Store`,
-      });
-      const marker = new window.google.maps.Marker({
-        position: coordinates,
-        map: googleMap.current,
-        id: edge.node.id
+      distanceFromPerth(coordinates).then((distance) => {
+        const infowindow = new window.google.maps.InfoWindow({
+          content: `<div class='font-bold text-base mb-2 text-orange-700'>${edge.node.name} Store.</div><div class='font-bold text-sm mb-2 text-gray-700'> ${distance} from Perth CBD.</div>`,
+        });
+        const marker = new window.google.maps.Marker({
+          position: coordinates,
+          map: googleMap.current,
+          id: edge.node.id
+        })
+        marker.addListener('click', () => {
+          googleMap.current.panTo(coordinates)
+          googleMap.current.setZoom(16)
+          infowindow.open(googleMap.current, marker)
+          props.onSelectStore(marker.id)
+          setOpenInfoWindow(infowindow)
+        })
+        markerArray.push(marker)
       })
-      marker.addListener('click', () => {
-        googleMap.current.panTo(coordinates)
-        googleMap.current.setZoom(16)
-        infowindow.open(googleMap.current, marker)
-        props.onSelectStore(marker.id)
-        setOpenInfoWindow(infowindow)
-      })
-      markerArray.push(marker)
+
       bounds.extend(coordinates)
     })
     googleMap.current.fitBounds(bounds)
     setMarkersArray(markerArray)
   }
-  
+
   useEffect(() => {
     const googleMapScript = document.createElement('script');
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GATSBY_GOOGLE_API_KEY}`
@@ -54,10 +57,10 @@ function Map(props) {
     if (props.selectedId) {
       const locationMarker = markersArray.filter((edge) => { return edge.id === props.selectedId })
       openInfoWindow && openInfoWindow.close()
-      new window.google.maps.event.trigger( locationMarker[0], 'click' )
+      new window.google.maps.event.trigger(locationMarker[0], 'click')
     }
-    
-  }, [props.selectedId]);  
+
+  }, [props.selectedId]);
 
   const { allLocation } = useStaticQuery
     (graphql`
@@ -74,6 +77,31 @@ function Map(props) {
           }
       }
   `)
+
+  const distanceFromPerth = async (coordinates) => {
+
+    const calcDistance = (service, data) => new Promise((resolve, reject) => {
+      service.getDistanceMatrix(data, (response, status) => {
+        if (status === 'OK') {
+          resolve(response.rows[0].elements[0].distance.text)
+        } else {
+          reject('Error was: ' + status);
+        }
+      })
+    })
+
+    const result = await calcDistance(
+      new window.google.maps.DistanceMatrixService,
+      {
+        origins: ['CBD, Western Australia'],
+        destinations: [coordinates],
+        travelMode: 'DRIVING',
+        unitSystem: window.google.maps.UnitSystem.METRIC
+      }
+    )
+
+    return result
+  }
 
   return (
     <div
--- src/components/StoreInfo/storeDetails.js
+++ src/components/StoreInfo/storeDetails.js
@@ -93,7 +93,7 @@ class StoreDetails extends Component {
 
     render() {
         return (
-            this.state.validSelection && <div>
+            this.state.validSelection && <div className=' text-gray-700'>
                 <div className='font-normal'>
                     ADDRESS:
                 </div>
@@ -113,7 +113,7 @@ class StoreDetails extends Component {
                     {this.state.days}
                 </div>
                 {(this.state.storeId === this.state.storePreference) && <div className='flex justify-end font-light text-md sm:text-lg'>
-                    <svg class='w-6 h-6 fill-current text-yellow-400' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                    <svg className='w-6 h-6 fill-current text-yellow-400' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
                         <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
                     </svg>
                     Favourite
@@ -123,7 +123,7 @@ class StoreDetails extends Component {
                         className='flex border-solid border-b-2 border-gray-600'
                         value={this.state.storeId}
                         onClick={this.onStorePrefChange}>
-                        <svg class='w-6 h-6 fill-current text-gray-400' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                        <svg className='w-6 h-6 fill-current text-gray-400' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
                             <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
                         </svg>
                         Set store as favourite?
